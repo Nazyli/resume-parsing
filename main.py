704d34eedb2deaad423de8d6dcffb3dac1e30964
Nazyli <evrynazyli@gmail.com>
2023-01-01 22:48:20 +0700

    add extract_names

--- main.py
+++ main.py
@@ -1,7 +1,31 @@
 import os
+from pdfminer.high_level import extract_text
+import nltk
+# nltk.download('stopwords')
+# nltk.download('punkt')
+# nltk.download('averaged_perceptron_tagger')
+# nltk.download('maxent_ne_chunker')
+# nltk.download('words')
 
 
-pathDir='input'
-onlyfiles = [os.path.join(pathDir, f) for f in os.listdir(pathDir) if os.path.isfile(os.path.join(pathDir, f))]
+def extract_text_from_pdf(pdf_path):
+    return extract_text(pdf_path)
+
+
+def extract_names(txt):
+    person_names = []
+    for sent in nltk.sent_tokenize(txt):
+        for chunk in nltk.ne_chunk(nltk.pos_tag(nltk.word_tokenize(sent))):
+            if hasattr(chunk, 'label') and chunk.label() == 'PERSON':
+                person_names.append(' '.join(chunk_leave[0] for chunk_leave in chunk.leaves()))
+
+    return person_names
+
+
+pathDir = 'input'
+onlyfiles = [os.path.join(pathDir, f) for f in os.listdir(
+    pathDir) if os.path.isfile(os.path.join(pathDir, f))]
 for x in onlyfiles:
-    print(x)
+    dataCV = extract_text_from_pdf(x)
+    names = extract_names(dataCV)
+    print(names)
